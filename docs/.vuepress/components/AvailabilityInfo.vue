1515d1f2b643bc19ae69618ee5df8e72e0e53705
hansemannn <hans.knoechel@hs-osnabrueck.de>
2018-11-22 09:40:59 +0100

    feat(availability): combine iOS platforms if both versions are equal

--- docs/.vuepress/components/AvailabilityInfo.vue
+++ docs/.vuepress/components/AvailabilityInfo.vue
@@ -2,7 +2,7 @@
   <div class="proxy-meta availability">
     <div class="proxy-meta-name">Availability</div>
     <ul>
-      <li v-for="platform in platforms">
+      <li v-for="platform in normalizedPlaforms(platforms)">
         <img :src="$withBase(imageForPlatform(platform.name))" class="platform-logo"/> <span>{{platform.since}}</span>
       </li>
     </ul>
@@ -15,11 +15,33 @@ export default {
     platforms: Array
   },
   methods: {
+    normalizedPlaforms(platforms) {
+      // Find the index of both platforms
+      const iphoneIndex = platforms.findIndex(platform => platform.name === 'iphone');
+      const ipadIndex = platforms.findIndex(platform => platform.name === 'ipad');
+
+      // If iPhone or iPad platform is not supported anyway, return early
+      if (iphoneIndex === -1 || ipadIndex === -1) {
+        return platforms;
+      }
+
+      // If both platforms exist and their versions match, compute the array
+      if (platforms[iphoneIndex].version === platforms[ipadIndex].version) {
+        const version = platforms[iphoneIndex].since;
+  
+        platforms.splice(iphoneIndex, 1);
+        platforms.splice(platforms.findIndex(platform => platform.name === 'ipad'), 1); // use again since index changed
+        platforms.push({ name: 'ios', pretty_name: 'iOS', since: version });
+      }
+
+      return platforms;
+    },
     imageForPlatform(platformName) {
       switch (platformName) {
         case 'android': return '/android-logo.png';
-        case 'iphone':
-        case 'ipad': return '/apple-logo.png';
+        case 'ios': return '/apple-logo.png'
+        case 'iphone': return '/iphone-logo.png'
+        case 'ipad': return '/ipad-logo.png';
         case 'windowsphone': return '/windows-logo.png';
       }
     }
