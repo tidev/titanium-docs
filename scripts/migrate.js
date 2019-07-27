6a454bf52f37ff58a14cdb0482d01bce3e2e721b
Jan Vennemann <jan.vennemann@gmx.net>
2019-07-27 15:36:47 +0200

    refactor: replace local plugins and theme with npm packages

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -1,24 +1,23 @@
 const fs = require('fs-extra');
 const path = require('path');
 const semver = require('semver');
-const { typesMetadata, getLinkForKeyPath } = require('../docs/.vuepress/utils/metadata.js');
+const { metadataService } = require('vuepress-plugin-apidocs/lib/metadata/service.js');
 
 const createdTypes = [];
 const createdPseudoTypes = [];
 
-const targetVersion = '8.0.0';
+const targetVersion = '8.1.0';
+metadataService.loadMetadata({}, { sourceDir: path.resolve(__dirname, '..', 'docs') }, []);
 
 console.log(`Generating missing markdown files (Version: ${targetVersion})`);
 
-for (let typeName in typesMetadata) {
-  const metadata = typesMetadata[typeName];
+for (let typeName in metadataService.metadata.next) {
+  const metadata = metadataService.metadata.next[typeName];
   if (metadata.deprecated && metadata.deprecated.removed && semver.lte(metadata.deprecated.removed, targetVersion)) {
     continue;
   }
   let description = metadata.description || '';
   description = description.replace(/^(#{4,})/gm, (match, heading) => heading.substring(0, heading.length - 1));
-  const typeNameParts = metadata.name.split('.');
-  const breadcrumbLabel = typeNameParts[typeNameParts.length -1];
   const overview = description.length > 0 ? `\n\n## Overview\n\n${description.trim().replace(/^\s+|\s+$/g, '')}\n\n` : '\n\n'
   let examples = '';
   if (metadata.examples && metadata.examples.length > 0) {
@@ -37,7 +36,7 @@ for (let typeName in typesMetadata) {
 <ProxySummary/>${overview}${examples}<ApiDocs/>
 `;
 
-  //if (!fs.existsSync(destPath)) {
+  if (!fs.existsSync(destPath)) {
     fs.ensureDirSync(path.dirname(destPath));
     fs.writeFileSync(destPath, mdContent);
     if (metadata.type !== 'pseudo') {
@@ -45,7 +44,7 @@ for (let typeName in typesMetadata) {
     } else {
       createdPseudoTypes.push(metadata.name);
     }
-  //}
+  }
 }
 
 console.log(`Created the initial markdown files for ${createdTypes.length} actual types and ${createdPseudoTypes.length} pseudo types.`);
