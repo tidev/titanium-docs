0b558a34086fccc94fdfb915e853e27cbf232f5a
Jan Vennemann <jan.vennemann@gmx.net>
2020-10-09 14:48:55 +0200

    feat: finalize landing page

--- docs/.vuepress/local-components/landing/DiscoverModules.vue
+++ docs/.vuepress/local-components/landing/DiscoverModules.vue
@@ -1,57 +1,43 @@
 <template>
-  <section class="landing-content platform">
-    <div class="center">
-      <h2>Module Ecosystem</h2>
-      <p>Need a third-party native library in your app? Titanium modules have your covered. Choose from a wide selection of community modules or our premium support integrations.</p>
-      <ul class="module-examples">
-        <li>
-          <a href="https://github.com/hansemannn/titanium-firebase" class="firebase">
-            <img :src="$withBase('/images/firebase-logo.png')" />
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/appcelerator-modules/ti.map">
-            <img :src="$withBase('/images/google-maps-logo.png')" />
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <img :src="$withBase('/images/firebase-logo.png')" />
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/appcelerator-modules/ti.facebook">
-            <img :src="$withBase('/images/facebook-logo.png')" />
-          </a>
-        </li>
-      </ul>
-      <a href="">Discover modules</a>
+  <section class="container my-32 px-6 xl:px-12 mx-auto">
+    <div class="grid grid-cols-12 gap-4 mx-auto text-center mb-12">
+      <div class="col-start-2 col-span-10">
+        <header class="text-3xl lg:text-4xl font-bold mb-8">Module Ecosystem</header>
+        <p class="md:text-xl">Need a third-party native library in your app? Titanium modules have your covered. Choose from a wide selection of community modules or our premium support integrations.</p>
+      </div>
+    </div>
+    <div class="module-examples grid grid-cols-2 md:grid-cols-4 gap-8">
+      <div v-for="module in modules" class="flex items-center justify-center">
+        <a :href="module.link" class="flex flex-col lg:flex-row items-center justify-center rounded-full w-24 h-24 lg:w-48 lg:h-48 px-4" target="_blank">
+          <font-awesome-icon v-if="Array.isArray(module.icon)" :icon="module.icon" size="3x" />
+          <img v-else :src="$withBase(module.icon)" />
+          <span v-if="module.name" class="hidden lg:inline ml-2">{{ module.name }}</span>
+        </a>
+      </div>
     </div>
   </section>
 </template>
 
+<script>
+export default {
+  data: () => ({
+    modules: [
+      { icon: 'images/google-maps-logo.png', link: './api/modules/map.html' },
+      { name: 'ImageView', icon: ['fas', 'image'], link: 'https://github.com/AndreaVitale/av.imageview' },
+      { icon: 'images/facebook-logo.png', link: '' },
+      { icon: 'images/lottie-logo.png', link: 'https://github.com/m1ga/ti.animation#readme' }
+    ]
+  })
+}
+</script>
+
 <style lang="stylus">
 .module-examples
-  display flex
-  list-style none
-  padding 20px 0 40px 0
-
-  li
-    display flex
-    flex 1 1 auto
-    justify-content center
-    padding 10px
-
-    a
-      display flex
-      align-items center
-      justify-content center
-      width 150px
-      height 150px
-      border-radius 75px
-      box-shadow 0 6px 24px rgba(0, 0, 0, .1)
-      img
-        max-height 60%
-      &.firebase
-        background-color #159BE5
+  a
+    box-shadow: 0 0 15px -3px rgba(0, 0, 0, 0.1), 0 0 6px -2px rgba(0, 0, 0, 0.05);
+    color: $textColor
+    &:hover
+      text-decoration none !important
+    img
+      max-height: 60%
 </style>
---
+++ docs/.vuepress/local-components/landing/SignUpCall.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="cta bg-red-500 grid grid-cols-12 mx-auto text-center py-20 relative">
+    <div class="col-span-12 md:col-span-8 md:col-start-3 text-center text-white">
+      <header class="text-4xl lg:text-5xl font-bold">Start free, grow from there.</header>
+      <p class="text-xl lg:text-2xl py-6">
+        Titanium is part of Axway's AMPLIFY integration platform. Sign up for free to start building your own great mobile apps today, or explore pricing to access the full suite of tools and features Titanium has to offer.
+      </p>
+      <p class="pt-4 mb-4">
+        <a class="btn btn-lg btn-white btn-pill mr-4" href="https://platform.axway.com/#/signup" target="_blank">Sign Up Free</a>
+        <a class="btn btn-lg btn-white btn-pill btn-outline" href="https://appcelerator.com/pricing/" target="_blank">Explore Pricing</a>
+      </p>
+    </div>
+  </section>
+</template>
+
+<style lang="stylus">
+.cta
+  background-image url('../../public/images/icons/icon-titanium-red.svg')
+  background-position 50% 40%
+  background-repeat no-repeat
+  background-size 60%
+</style>
