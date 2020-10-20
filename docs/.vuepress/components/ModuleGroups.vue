ec75a03cc89bfa4a3c8716c237eacb8a7ee270f4
Jan Vennemann <jan.vennemann@gmx.net>
2020-10-20 20:53:53 +0200

    feat: quick access module cards for common apis

--- docs/.vuepress/components/ModuleGroups.vue
+++ docs/.vuepress/components/ModuleGroups.vue
@@ -1,127 +1,204 @@
 <template>
   <section>
-    <h3>User Interface</h3>
-    <div class="grid grid-cols-2 gap-6 mt-8 mb-16">
+    <module-group title="User Interface">
       <module-card
         title="UI"
         icon="layer-group"
-        link="/api/titanium/ui.html"
+        link="./titanium/ui.html"
       >
         Root namespace for UI related code/widgets
       </module-card>
       <module-card
-        title="DisplayCaps"
-        icon="mobile-alt"
-        link="/api/titanium/platform/displaycaps.html"
+        title="View"
+        icon="window-restore"
+        link="./titanium/ui/view.html"
       >
-        Details on the specific hardware densities, dimensions of the device
+        The base type for UI elements in Titanium and building block for your own UI.
       </module-card>
       <module-card
-        title="Locale"
-        icon="language"
-        link="/api/titanium/locale.html"
+        title="Label"
+        icon="align-left"
+        link="./titanium/ui/label.html"
       >
-        APIs for formatting text, numbers, currency for locales and setting current locale.
+        Fully stylable label to display one or multiple lines of text.
       </module-card>
       <module-card
-        title="Intl"
-        icon="globe-americas"
-        link="/api/global/intl.html"
+        title="List View"
+        icon="th-list"
+        link="./titanium/ui/listview.html"
       >
-        Web compatible Intl APIs for formatting dates, times, numbers and sorting strings.
+        A list view is used to present rows of information, such as an item list or menu.
       </module-card>
-    </div>
+    </module-group>
 
-    <h3>Network, Filesystem and I/O</h3>
-    <div class="grid grid-cols-2 gap-6 mt-8 mb-16">
+    <module-group title="Network, Filesystem and I/O">
       <module-card
         title="Network"
         icon="wifi"
-        link="/api/titanium/network.html"
+        link="./titanium/network.html"
       >
         Root namespace for network related code, such as an <router-link to="./titanium/network/httpclient.html">HTTP client</router-link>.
       </module-card>
       <module-card
         title="Filesystem"
         icon="file-alt"
-        link="/api/titanium/filesystem.html"
+        link="./titanium/filesystem.html"
       >
-        Root namespace for interacting with the filesystem
+        Root namespace for interacting with the filesystem.
       </module-card>
       <module-card
         title="Stream"
         icon="stream"
-        link="/api/titanium/stream.html"
+        link="./titanium/stream.html"
       >
-        Utility methods for reading/writing to/from streams
+        Utility methods for reading/writing to/from streams.
       </module-card>
       <module-card
         title="Buffer"
         icon="memory"
-        link="/api/titanium/buffer.html"
+        link="./titanium/buffer.html"
       >
-        A read/write instance of bytes typically used in conjunction with streams
+        A read/write instance of bytes typically used in conjunction with streams.
       </module-card>
-    </div>
+    </module-group>
 
-    <h3>Sensors</h3>
-    <div class="grid grid-cols-2 gap-6 mt-8 mb-16">
+    <module-group title="Media">
+      <module-card
+        title="Media"
+        icon="photo-video"
+        link="./titanium/media.html"
+      >
+        Access the device's media-related functionality, such as the device's <router-link to="./titanium/media.html#showcamera">camera</router-link>  or photo <router-link to="./titanium/media.html#openphotogallery">gallery</router-link>.
+      </module-card>
+      <module-card
+        title="Audio Player"
+        icon="play"
+        link="./titanium/media/audioplayer.html"
+      >
+        Stream audio to the device and control playback.
+      </module-card>
+      <module-card
+        title="Music Player"
+        icon="music"
+        link="./titanium/media/musicplayer.html"
+      >
+        Lets you manage and playback a queue of <router-link to="./titanium/media/item.html">media objects</router-link>.
+      </module-card>
+      <module-card
+        title="Video Player"
+        icon="film"
+        link="./titanium/media/videoplayer.html"
+      >
+        Play local video files or stream from a remote server, either in fullscreen or embedded in other views.
+      </module-card>
+    </module-group>
+
+    <module-group title="Sensors">
       <module-card
         title="Accelerometer"
-        icon="layer-group"
-        link="/api/titanium/accelerometer.html"
+        icon="atom"
+        link="./titanium/accelerometer.html"
       >
         Register for events when the device's x/y/z rotation changes.
       </module-card>
       <module-card
         title="Geolocation"
         icon="map-marker-alt"
-        link="/api/titanium/geolocation.html"
+        link="./titanium/geolocation.html"
       >
         Requesting and accessing location coordinates and geocoding addresses.
       </module-card>
       <module-card
         title="Gesture"
         icon="expand-alt"
-        link="/api/titanium/gesture.html"
+        link="./titanium/gesture.html"
       >
         Query for device orientation and register for orientation changes or shake events.
       </module-card>
-    </div>
+    </module-group>
 
-    <h3>SQLite Database</h3>
-    <div class="grid grid-cols-2 gap-6 my-5">
+    <module-group title="SQLite Database">
       <module-card
         title="Database"
         icon="layer-group"
-        link="/api/titanium/database.html"
+        link="./titanium/database.html"
       >
         Root namespace for APIs to manage Databases
       </module-card>
       <module-card
         title="DB"
         icon="database"
-        link="/api/titanium/database/db.html"
+        link="./titanium/database/db.html"
       >
         An instance of a Database
       </module-card>
       <module-card
         title="ResultSet"
         icon="poll-h"
-        link="/api/titanium/database/resultset.html"
+        link="./titanium/database/resultset.html"
       >
         Result Sets from querying/manipulating a Database
       </module-card>
-    </div>
+    </module-group>
+
+    <module-group title="Utilities">
+      <module-card
+        title="DisplayCaps"
+        icon="expand"
+        link="./titanium/platform/displaycaps.html"
+      >
+        Provides details on the specific hardware densities and dimensions of the device
+      </module-card>
+      <module-card
+        title="App"
+        icon="mobile-alt"
+        link="./titanium/app.html"
+      >
+        Access app-specific data at runtime, such as name, id, or version and for sending or listening for system events.
+      </module-card>
+      <module-card
+        title="Properties"
+        icon="hdd"
+        link="./titanium/app/properties.html"
+      >
+        Store app related data that persists beyond app sessions and device power cycles.
+      </module-card>
+      <module-card
+        title="Utils"
+        icon="cog"
+        link="./titanium/utils.html"
+      >
+        Utility for base64 encode/decode, as well as sha/md5 hashing
+      </module-card>
+    </module-group>
+
+    <module-group title="Logging">
+      <module-card
+        title="API"
+        icon="align-justify"
+        link="./titanium/api.html"
+      >
+        A Titanium-specific logger. For developers used to the Web or Node.JS, you're probably looking for <router-link to="./global/console.html">console</router-link> instead.
+      </module-card>
+      <module-card
+        title="Analytics"
+        icon="search"
+        link="./titanium/analytics.html"
+      >
+        Built in APIs for recording app-specific analytics, to record useful data/events from your app in the Platform Dashboard backend.
+      </module-card>
+    </module-group>
   </section>
 </template>
 
 <script>
 import ModuleCard from '@components/api/ModuleCard'
+import ModuleGroup from '@components/api/ModuleGroup'
 
 export default {
   components: {
-    ModuleCard
+    ModuleCard,
+    ModuleGroup
   }
 }
 </script>
--- docs/api/README.md
+++ docs/api/README.md
@@ -13,58 +13,9 @@ const button = Titanium.UI.createButton({ title: 'Send' });
 const win = Ti.UI.createWindow({ title: 'Home' });
 ```
 
-You'll find all APIs arranged under this root namespace. Browse the full list of available APIs via the sidebar to the left, check out the most frequently used ones in the groups below.
+You'll find all APIs arranged under this root namespace. Browse the full list of available APIs via the sidebar to the left or check out the most frequently used ones in the groups below.
 
 <ModuleGroups />
-
-### User Interface
-
-- [UI](/api/titanium/ui/) - Root namespace for UI related code/widgets
-- [DisplayCaps](/api/titanium/platform/displaycaps) - Details on the specific hardware densities, dimensions of the device
-- [Locale](/api/titanium/locale) - APIs for formatting text, numbers, currency for locales and setting current locale. Most commonly used method [Ti.Locale.getString](/api/titanium/locale#getstring) is aliased as a global [L](/api/global#l) function.
-- [Intl](/api/global/intl) - Web comaptible Intl APIs for formatting dates, times, numbers and sorting strings.
-
-### Sensors
-
-- [Accelerometer](/api/titanium/accelerometer/) - register for events when the device's x/y/z rotation changes.
-- [Geolocation](/api/titanium/geolocation/) - requesting and accessing location coordinates and geocoding addresses.
-- [Gesture](/api/titanium/gesture/) - query for device orientation and register for orientation changes or shake events.
-
-### SQLite Database
-
-- [Database](/api/titanium/database/) - root namespace for APIs to manage Databases
-- [DB](/api/titanium/database/db/) - An instance of a Database
-- [ResultSet](/api/titanium/database/resultset/) - Result Sets from querying/manipulating a Database
-
-### Network, Filesystem and I/O
-
-- [Network](/api/titanium/network/) - Root namespace for network related code, such as an HTTP client.
-- [Filesystem](/api/titanium/filesystem/) - Root namespace for interacting with the filesystem
-- [Blob](/api/titanium/blob/) - A read-only instance of bytes representing binary or text data (such as images or file contents)
-- [Buffer](/api/titanium/buffer/) - A read/write instance of bytes typically used in conjunction with streams
-- [Codec](/api/titanium/codec/) - Utility methods for encoding/decoding bytes in buffers
-- [Stream](/api/titanium/stream/) - Utility methods for reading/writing to/from streams
-- [IOStream](/api/titanium/iostream/) -The base interface for streams
-
-### Logging
-
-- [API](/api/titanium/api/) - A Titanium-specific logger. For most developers used to the Web or Node.JS, they;d typically use [console](/api/globals/console/) instead.
-- [Analytics](/api/titanium/analytics) - Built in APIs for recording app-specific analytics, to record useful data/events from your app in the Platform Dashboard backend.
-
-### Utilities
-
-- [App](/api/titanium/app/) - Root namespace for accessing app-specific data, such as: name, id, guid, versionm, etc. Also used to register for app-wide events such as pause/resume.
-- [Properties](/api/titanium/app/properties/) - Read/write of app-specific properties
-- [Utils](/api/titanium/utils/) - utility methods for base64 encode/decode, as well as sha/md5 hashing.
-
-### Contacts, Calendars
-
-- [Contacts](/api/titanium/contacts/) - Querying and managing contacts
-- [Calendar](/api/titanium/calendar/) - Querying and managing calendars and events
-
-### Audio and Video
-
-- [Media](/api/titanium/media/)
 
 ## Modules
 
