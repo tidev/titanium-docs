2f44ac578dae5ecef567cc7e725ccdb1464eda5a
Chris Williams <chris.a.williams@gmail.com>
2020-11-18 14:29:37 -0500

    perf: tweak purgecss

--- docs/.vuepress/plugins/purgecss.js
+++ docs/.vuepress/plugins/purgecss.js
@@ -14,16 +14,29 @@ module.exports = (options, context) => {
   const purgecss = require('@fullhuman/postcss-purgecss')
   siteConfig.postcss.plugins.push(purgecss({
     content: [
-      // `${sourceDir}/**/*.md`,
-      `${sourceDir}/api/**/*.md`,
+      // Retain any css classes from any vue component used
       `${sourceDir}/.vuepress/components/*.vue`,
       `${sourceDir}/.vuepress/local-components/**/*.vue`,
-      `${sourceDir}/../website/pages/**/*.md`,
       `${sourceDir}/../website/pages/**/*.vue`,
       `node_modules/@vuepress/plugin-*/**/*.vue`,
       `node_modules/@vuepress/core/lib/**/*.vue`,
       `node_modules/vuepress-plugin-*/**/*.vue`,
-      `node_modules/vuepress-theme-*/**/*.vue`
+      `node_modules/vuepress-theme-*/**/*.vue`,
+      // Retain all styles from the theme
+      `node_modules/vuepress-theme-titanium/styles/*.styl`,
+      // and local override(s)
+      `${sourceDir}/.vuepress/styles/*.styl`
+    ],
+    extractors: [
+      {
+        extractor: content => {
+          const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, "");
+          return (
+            contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
+          );
+        },
+        extensions: [ 'vue' ],
+      },
     ],
     defaultExtractor: content => {
       // Capture as liberally as possible, including things like `h-(screen-1.5)`
@@ -33,7 +46,14 @@ module.exports = (options, context) => {
       const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
 
       return broadMatches.concat(innerMatches)
-    }
+    },
+    safelist: [
+      /-(leave|enter|appear)(|-(to|from|active))$/,
+      /^(?!(|.*?:)cursor-move).+-move$/,
+      /^router-link(|-exact)-active$/,
+      /data-v-.*/,
+    ],
+    rejected: true
   }))
 
   return plugin
